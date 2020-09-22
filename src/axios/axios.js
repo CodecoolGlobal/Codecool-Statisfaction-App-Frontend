dd7a6e720726aa3a57495f4cc1812bba834c564e
Máté Iszlai <iszimate@gmail.com>
2020-09-22 20:02:20 +0200

    Create axios

---
+++ src/axios/axios.js
@@ -0,0 +1,10 @@
+import Axios from "axios";
+
+const API_URL = "http://localhost:5000/api/";
+const TIMEOUT = 5000;
+
+export default Axios.create({
+  baseURL: API_URL,
+  timeout: TIMEOUT,
+  withCredentials: true,
+});
