edd86499cf490565b9ddd01121389cd68cb946e7
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2020-09-24 13:17:12 +0200

    fix logout button

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -4,12 +4,13 @@ import '../Components/Sidebar.css'
 import { logout } from "../Api/AuthCalls";
 
 function Sidebar() {
+
     return (
         <div className="sidebar">
             <SidebarOption active text="Home"/>
             <SidebarOption text="Questions"/>
             <SidebarOption text="Profile"/>
-            <SidebarOption onClick={()=> logout()} text="Logout"/>
+            <SidebarOption onClick={logout()} text="Logout"/>
         </div>
     )
 }
--- src/Components/SidebarOption.js
+++ src/Components/SidebarOption.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import '../Components/SidebarOption.css'
 
-function SidebarOption({ text, active }) {
+function SidebarOption({ text, active, onClick }) {
     return (
-        <div className={`sidebarOption ${active && 'sidebarOption--active'}`}>
+        <div className={`sidebarOption ${active && 'sidebarOption--active'}`} onClick={onClick}>
             <h2>{text}</h2>
         </div>
     )
