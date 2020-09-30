eb2528d44c6e64096339fa3f283cc5594dfd3ae0
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-09-30 13:32:37 +0200

    Create InfoBar

---
+++ src/Components/InfoBar/InfoBar.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./InfoBar.css";
+
+export default function InfoBar() {
+  return (
+    <div className="infobar">
+      <h3>info info info</h3>
+    </div>
+  );
+}
