3eb241c86f3f15a417cf7d8520e303626993c81c
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-01 19:21:21 +0200

    Create survey list for Questions page

--- src/Components/QuestionFeed.js
+++ src/Components/QuestionFeed.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "../Components/QuestionFeed.css";
 import PageHeader from "./PageHeader/PageHeader";
+import Survey from "./Survey/Survey";
 
 function QuestionFeed() {
   return (
     <div className="wall">
       <PageHeader title={"QuestionFeed"} />
-      <div>
-        <h2>QuestionFeed</h2>
+      <div className="survey-feed-container">
+        <Survey title="Weekly survery 01" date="01.10.2020" isDone />
+        <Survey title="Weekly survery 02" date="28.09.2020" isDOne="false" />
+        <Survey title="Weekly survery 03" date="23.09.2020" isDOne="false" />
       </div>
     </div>
   );
