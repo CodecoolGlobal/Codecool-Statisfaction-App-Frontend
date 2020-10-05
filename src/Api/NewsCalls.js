6b07c0ba17666379379d1801436bc82da9566a51
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-05 19:03:27 +0200

    Update dummy date

--- src/Api/NewsCalls.js
+++ src/Api/NewsCalls.js
@@ -2,15 +2,15 @@ export default async function getNews() {
   return [
     {
       id: 1,
-      author: "András Terray",
+      author: "László Terray",
       date: "2020.10.17.",
       post:
-        "Please Everyone make sure that you fill out the weekly survey as well as giving us some feedback. Hope you have a nice day!",
+        "Based on your feedbacks from last week we set up a new interactive frontend live-coding session for every wednesday with one of our mentors, so you can improve your frontend skills. I will send out the details via email later this day.",
     },
     {
       id: 2,
       author: "Gábor Dáher",
-      date: "2020.10.3.",
+      date: "2020.10.13.",
       post:
         "Hi! I want to say thanks for the satisfaction app development team for their amazing work, this is exactly the application I wanted. Hope you guys will use it regulary and give us as much feed back you can. Thank you!",
     },
@@ -19,7 +19,7 @@ export default async function getNews() {
       author: "Gyula Hódosi",
       date: "2020.09.29.",
       post:
-        "I finally figured out how to use this ux designer tool so now I can make a wireframe prototype of our website. I hope my development team will like it. It still doesn't do the things I want sometimes.Good luck team!",
+        "Based on the meeting with the client last week, we have made a wireframe and a prototype of the codecool satisfaction app that reflects the features we plan to develop.",
     },
   ];
 }
