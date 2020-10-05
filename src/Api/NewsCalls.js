5d2776b32b7cfbae3ca97c66081d39cf8a4125f3
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-05 15:41:57 +0200

    Create api call for news with dummy data

---
+++ src/Api/NewsCalls.js
@@ -0,0 +1,25 @@
+export default async function getNews() {
+  return [
+    {
+      id: 1,
+      author: "András Terray",
+      date: "2020.10.17.",
+      post:
+        "Please Everyone make sure that you fill out the weekly survey as well as giving us some feedback. Hope you have a nice day!",
+    },
+    {
+      id: 2,
+      author: "Gábor Dáher",
+      date: "2020.10.3.",
+      post:
+        "Hi! I want to say thanks for the satisfaction app development team for their amazing work, this is exactly the application I wanted. Hope you guys will use it regulary and give us as much feed back you can. Thank you!",
+    },
+    {
+      id: 3,
+      author: "Gyula Hódosi",
+      date: "2020.09.29.",
+      post:
+        "I finally figured out how to use this ux designer tool so now I can make a wireframe prototype of our website. I hope my development team will like it. It still doesn't do the things I want sometimes.Good luck team!",
+    },
+  ];
+}
