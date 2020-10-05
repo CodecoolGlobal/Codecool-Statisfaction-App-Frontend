669369534bcbfcbfd62661cdef2d2a6ca17c9378
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-05 19:03:49 +0200

    Fixed typo in title text

--- src/Components/QuestionFeed.js
+++ src/Components/QuestionFeed.js
@@ -6,11 +6,11 @@ import Survey from "./Survey/Survey";
 function QuestionFeed() {
   return (
     <div className="wall">
-      <PageHeader title={"QuestionFeed"} />
+      <PageHeader title={"Surveys"} />
       <div className="survey-feed-container">
-        <Survey title="Weekly survery 01" date="01.10.2020" isDone />
-        <Survey title="Weekly survery 02" date="28.09.2020" isDOne="false" />
-        <Survey title="Weekly survery 03" date="23.09.2020" isDOne="false" />
+        <Survey title="Weekly survey 01" date="01.10.2020" isDone />
+        <Survey title="Weekly survey 02" date="28.09.2020" isDOne="false" />
+        <Survey title="Weekly survey 03" date="23.09.2020" isDOne="false" />
       </div>
     </div>
   );
