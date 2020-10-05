01761c049ecb1f90229192a66696aa3bd8f4106f
Máté Iszlai <iszimate@gmail.com>
2020-10-05 13:32:19 +0200

    Add button and dialog to add new feedback (dummy functions, no backend connection)

--- src/Components/Feedbacks/Feedbacks.js
+++ src/Components/Feedbacks/Feedbacks.js
@@ -1,13 +1,54 @@
+import {
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  DialogTitle,
+  FormControlLabel,
+  Switch,
+  TextField,
+} from "@material-ui/core";
 import React, { useEffect, useState } from "react";
-import { GetFeedbacks } from "../../Api/FeedbacksCalls";
+import { GetFeedbacks, PostFeedback } from "../../Api/FeedbacksCalls";
 import Feedback from "../Feedback/Feedback";
 import PageHeader from "../PageHeader/PageHeader";
 import "./Feedbacks.css";
 
 export default function Feedbacks() {
   const [feedbacks, setFeedbacks] = useState([]);
+  const [showAdd, setShowAdd] = useState(false);
+  const [feedbackMessage, setFeedbackMessage] = useState("");
+  const [anonymus, setAnonymus] = useState(false);
 
   const handleVote = (vote) => {};
+
+  const handleClose = () => {
+    setShowAdd(false);
+    setFeedbackMessage("");
+    setAnonymus(false);
+  };
+
+  const handleSave = () => {
+    setShowAdd(false);
+    var today = new Date();
+    let [month, day, year] = today.toLocaleDateString().split("/");
+
+    let feedback = {
+      id: -1,
+      user: anonymus ? "anonymus" : "username", //localStorage.getItem("username"),
+      title: feedbackMessage,
+      date: `${year}-${month}-${day}`,
+      votes: 1,
+    };
+    async function SendFeedback(feedback) {
+      let feedbackId = await PostFeedback(feedback);
+      if (feedbackId > 0) {
+        feedback["id"] = feedbackId;
+        setFeedbacks(feedbacks.concat(feedback));
+      }
+    }
+    SendFeedback(feedback);
+  };
 
   useEffect(() => {
     async function FetchFeedbacks() {
@@ -16,6 +57,7 @@ export default function Feedbacks() {
     }
     FetchFeedbacks();
   }, []);
+
   return (
     <div className="wall">
       <PageHeader title={"Feedbacks"} />
@@ -35,6 +77,52 @@ export default function Feedbacks() {
           ))
         )}
       </div>
+      <div className="button-container">
+        <Button
+          id="add-feedback-btn"
+          variant="contained"
+          color="primary"
+          onClick={() => setShowAdd(true)}
+        >
+          Add feedback
+        </Button>
+      </div>
+      <Dialog
+        open={showAdd}
+        onClose={handleClose}
+        aria-labelledby="add-dialog-title"
+      >
+        <DialogTitle id="add-dialog-title">Add feedback</DialogTitle>
+        <DialogContent>
+          <TextField
+            autoFocus
+            margin="dense"
+            id="title-input"
+            label="Feedback message"
+            multiline
+            fullWidth
+            onChange={(event) => setFeedbackMessage(event.target.value)}
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                checked={anonymus}
+                onChange={(event) => setAnonymus(event.target.checked)}
+                color="primary"
+              />
+            }
+            label="anonymus"
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button onClick={handleSave} color="primary">
+            Add feedback
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
