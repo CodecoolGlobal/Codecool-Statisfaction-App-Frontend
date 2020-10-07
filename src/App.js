8c3b18181b850bcbfda1e5c7809e82a95573ad10
Barnabas, Bucsek <bucsek.barnabas@gmail.com>
2020-10-07 10:48:44 +0200

    delete pages dict and files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
-import HomePage from "./Pages/HomePage";
-import ProfilePage from "./Pages/ProfilePage";
-import AuthPage from "./Pages/AuthPage";
 import AuthCheck from "./Util/AuthCheck";
-import QuestionsPage from "./Pages/QuestionsPage";
 import Header from "./Components/Header/Header";
-import FeedbacksPage from "./Pages/FeedbacksPage/FeedbacksPage";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import InfoBar from "./Components/InfoBar/InfoBar";
 import Feedbacks from "./Components/Feedbacks/Feedbacks";
 import NewsFeed from "./Components/NewsFeed";
 import Profile from "./Components/Profile/Profile";
 import QuestionFeed from "./Components/QuestionFeed";
-
+import Login from "./Components/Login"
 
 function App() {
   return (
   <div className="app">
     <BrowserRouter>
       <Sidebar />
-      <Route exact path="/login" component={AuthPage} />
+      <Route exact path="/login" component={Login} />
       <Route
         exact
         path="/feedbacks"
