59e16d909601fe06ad8fa4b50d0646f6e62ab95d
Barnabas, Bucsek <bucsek.barnabas@gmail.com>
2020-10-08 13:32:44 +0200

    create new profile page

--- src/Components/Profile/Profile.js
+++ src/Components/Profile/Profile.js
@@ -6,6 +6,12 @@ import LocationOn from "@material-ui/icons/LocationOn";
 import MenuBook from "@material-ui/icons/MenuBook";
 import Edit from "@material-ui/icons/Edit";
 import AccountBox from "@material-ui/icons/AccountBox";
+import InputLabel from "@material-ui/core/InputLabel";
+import MenuItem from "@material-ui/core/MenuItem";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Select from "@material-ui/core/Select";
+import { Button } from "@material-ui/core";
 
 function Profile() {
   const [user, setUser] = useState();
@@ -44,6 +50,10 @@ function Profile() {
     };
     changeCourse(obj);
   };
+
+  if (!user) {
+    return <div>loading...</div>;
+  }
 
   return (
     /* <div className="profile-container">
@@ -88,6 +98,56 @@ function Profile() {
     </div> */
 
     <div className="profile">
+      <div className="profile-card">
+        <img className="avatar" alt="user" src={picture}></img>
+        <div className="profile-field">
+          <AccountBox className="profile-icon" />
+          <div className="profile-field-info">
+            <div className="profile-field-info-title">Name:</div>
+            <div className="profile-field-info-name">
+              {`${user.firstName} ${user.lastName}`}
+            </div>
+          </div>
+        </div>
+        <div className="profile-field">
+          <MenuBook className="profile-icon" />
+          <div className="profile-field-info">
+            <div className="profile-field-info-title">Course:</div>
+            <div className="profile-field-info-name">
+              {`${user.courseName}`}
+            </div>
+          </div>
+        </div>
+        <div className="profile-field">
+          <LocationOn className="profile-icon" />
+          <div className="profile-field-info">
+            <div className="profile-field-info-title">City:</div>
+            <div className="profile-field-info-name">
+              {`${user.city == null ? "not yet added" : user.city}`}
+            </div>
+          </div>
+        </div>
+        <FormControl className="from">
+          <Select value={course} onChange={handleChange} displayEmpty>
+            <MenuItem value="" disabled>
+              {user.courseName && user.courseName}
+            </MenuItem>
+            {courses.map((course) => (
+              <MenuItem value={course.name} key={course.id}>
+                {course.name}
+              </MenuItem>
+            ))}
+          </Select>
+          <FormHelperText>Select course</FormHelperText>
+        </FormControl>
+        <Button
+          color="primary"
+          variant="contained"
+          onClick={submitCourseChange}
+        >
+          Save changes
+        </Button>
+      </div>
     </div>
   );
 }
