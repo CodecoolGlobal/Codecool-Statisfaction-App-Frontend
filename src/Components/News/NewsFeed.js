978c8a02833cfd3e8aef5474e54ab40e8c93e2ed
Máté Iszlai <iszimate@gmail.com>
2020-10-12 16:59:15 +0200

    Add admin state

--- src/Components/News/NewsFeed.js
+++ src/Components/News/NewsFeed.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import {getNews} from "../../Api/NewsCalls";
+import { getNews } from "../../Api/NewsCalls";
 import "./NewsFeed.css";
 import News from "./News";
 import PageHeader from "../PageHeader/PageHeader";
+import { isAdmin } from "../../Api/AuthCalls";
 
 function NewsFeed() {
   const [news, setNews] = useState([]);
+  const [admin, setAdmin] = useState(false);
 
   useEffect(() => {
     async function fetchNews() {
@@ -13,6 +15,14 @@ function NewsFeed() {
       setNews(result);
     }
     fetchNews();
+  }, []);
+
+  useEffect(() => {
+    async function FetchAdmin() {
+      let result = await isAdmin();
+      setAdmin(result);
+    }
+    FetchAdmin();
   }, []);
 
   return (
