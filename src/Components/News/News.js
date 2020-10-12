9fdd3f672c252970931ba6455ede000d53c84436
GyulaHodosi <gyula.g.hodosi@gmail.com>
2020-10-12 15:58:58 +0200

    Connect news page with database

--- src/Components/News/News.js
+++ src/Components/News/News.js
@@ -5,11 +5,11 @@ export default function News({ news }) {
   return (
     <div className="news-container">
       <div className="news-header">
-        <div className="news-author">{news.author}</div>
+        <div className="news-author">{news.userName}</div>
         <div className="news-date">{news.date}</div>
       </div>
       <div className="news-body">
-        <div className="news-post">{news.post}</div>
+        <div className="news-post">{news.description}</div>
       </div>
     </div>
   );
