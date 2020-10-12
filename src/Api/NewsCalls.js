0f32118eb33d338bce57045d9368dc46523bc74f
Máté Iszlai <iszimate@gmail.com>
2020-10-12 16:59:02 +0200

    Revrite getNews

--- src/Api/NewsCalls.js
+++ src/Api/NewsCalls.js
@@ -1,25 +1,7 @@
+import Axios from "axios";
+
 export async function getNews() {
-  return [
-    {
-      id: 1,
-      author: "László Terray",
-      date: "2020.10.17.",
-      post:
-        "Based on your feedbacks from last week we set up a new interactive frontend live-coding session for every wednesday with one of our mentors, so you can improve your frontend skills. I will send out the details via email later this day.",
-    },
-    {
-      id: 2,
-      author: "Gábor Dáher",
-      date: "2020.10.13.",
-      post:
-        "Hi! I want to say thanks for the satisfaction app development team for their amazing work, this is exactly the application I wanted. Hope you guys will use it regulary and give us as much feed back you can. Thank you!",
-    },
-    {
-      id: 3,
-      author: "Gyula Hódosi",
-      date: "2020.09.29.",
-      post:
-        "Based on the meeting with the client last week, we have made a wireframe and a prototype of the codecool satisfaction app that reflects the features we plan to develop.",
-    },
-  ];
+  let result = await Axios.get("/news");
+  if (result.status === 200) return result.data;
+  return null;
 }
